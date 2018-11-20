961a38b42fbf0d0e1c06bb4744b457986a3d0c17
Pavel Taruts <taruts@improveitgroup.com>
2018-11-20 16:01:54 +0700

    Базовое контекстное меню

--- src/App.js
+++ src/App.js
@@ -1,28 +1,135 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import {
+    Container,
+    Row,
+    Col,
+    Dropdown,
+    DropdownMenu,
+    DropdownToggle,
+    DropdownItem,
+} from 'reactstrap';
+
+import {IndexLinkContainer} from 'react-router-bootstrap';
 
 class App extends Component {
-  render() {
+    
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Container>
+                        
+                        <Row>
+                            <Col>
+
+                                <Dropdown isOpen={this.state.isOpen} toggle={this.toggle}>
+                                    <DropdownToggle>Toggle</DropdownToggle>
+                                    <DropdownMenu>
+                                        
+                                        <IndexLinkContainer to="/">
+                                            <DropdownItem>
+                                                Home
+                                            </DropdownItem>
+                                        </IndexLinkContainer>
+                                        
+                                        <IndexLinkContainer to="/about">
+                                            <DropdownItem>
+                                                About
+                                            </DropdownItem>
+                                        </IndexLinkContainer>
+                                        
+                                        <IndexLinkContainer to="/topics">
+                                            <DropdownItem>
+                                                Topics
+                                            </DropdownItem>
+                                        </IndexLinkContainer>
+                                        
+                                    </DropdownMenu>
+                                </Dropdown>
+
+                            </Col>
+                        </Row>
+                        
+                        <Row>
+                            <Col>
+                                <Route exact path="/" component={Home} />
+                                <Route path="/about" component={About} />
+                                <Route path="/topics" component={Topics} />
+                            </Col>
+                        </Row>
+                        
+                    </Container>
+                </div>
+            </Router>
+        );
+    }
+}
+
+function Home() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. !!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <h2>Home</h2>
+        </div>
     );
-  }
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Topics({ match }) {
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li>
+                    <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/components`}>Components</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+                </li>
+            </ul>
+
+            <Route path={`${match.path}/:topicId`} component={Topic} />
+            <Route
+                exact
+                path={match.path}
+                render={() => <h3>Please select a topic.</h3>}
+            />
+        </div>
+    );
+}
+
+function Topic({ match }) {
+    return (
+        <div>
+            <h3>{match.params.topicId}</h3>
+        </div>
+    );
 }
 
 export default App;
